c184dc73f5f265d41e89408a6326651c0f337a73
tomotomy <tomo1999724@gmail.com>
2020-04-01 18:51:31 +0900

    show image from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,36 +10,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: ImageScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class ImageScreen extends StatelessWidget {
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Widget makeImageGrid() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+      itemBuilder: (context, index) {
+        return ImageGridItem(index);
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('images grid'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
+      body: Container(
+        child: makeImageGrid()
       ),
     );
   }
 }
+
+class ImageGridItem extends StatefulWidget {
+  int _index;
+  ImageGridItem(int index) {
+    this._index = index;
+  }
+
+  @override
+  _ImageGridItemState createState() => _ImageGridItemState();
+}
+
+class _ImageGridItemState extends State<ImageGridItem> {
+
+  Uint8List imageFile;
+  StorageReference photosReference = FirebaseStorage.instance.ref().child('photos');
+
+  @override
+  void initState() {
+    super.initState();
+    getImage();
+  }
+
+
+  getImage() {
+    int MAX_SIZE = 7*1024*1024; 
+    photosReference.child("${widget._index}.png").getData(MAX_SIZE).then((data) {
+      this.setState(() {
+        imageFile = data;
+      });
+    }).catchError((error) {
+
+    });
+  }
+
+  Widget decideGridGridTileWidget() {
+    if (imageFile == null) {
+      return Center(child: Text('No Data'));
+    } else {
+      return Image.memory(
+        imageFile,
+        fit: BoxFit.cover,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: decideGridGridTileWidget()
+    );
+  }
+}
+
+
